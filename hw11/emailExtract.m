1e4fd3bdc1eb2333a3cbc078a9383b0e4f13893b
henryyang42 <henryyang42@gmail.com>
2014-12-30 00:55:34 +0800

    add he11

---
+++ hw11/emailExtract.m
@@ -0,0 +1,12 @@
+function output = emailExtract(url)
+                  html = urlread(url)
+                         html = regexprep(html, '<!--[\S\s]*?-->', '');
+email_pattern = '([\w_]*\.)*[\w_]*@([\w_]*\.)*[\w_]*';
+[st, ed] = regexp(html, email_pattern);
+email_set = {};
+for i = 1 : length(st)
+            email = html(st(i) : ed(i));
+email_set {i} = email;
+end
+output = unique(email_set);
+end
